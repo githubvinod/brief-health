330923aac8372f120f334e570c3e2ef40c4a20e0
Vinod <vinod.g@citrusinformatics.com>
2019-06-18 11:27:49 +0530

    Edit in go file to get post from dropmark

--- support/tasks/build.go
+++ support/tasks/build.go
@@ -52,7 +52,7 @@ func main() {
 		panic(err)
 	}
 
-	if err = publisher.Publish(ctx, "https://shah.dropmark.com/616548.json"); err != nil {
+	if err = publisher.Publish(ctx, "https://informvinu.dropmark.com/684931.json"); err != nil {
 		panic(err)
 	}
 }
